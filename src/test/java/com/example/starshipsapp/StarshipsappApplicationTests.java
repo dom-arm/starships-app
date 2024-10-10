59710dbda6caf780aeb2c6c56d3bd2d369fc2f36
do ar <118741935+dom-arm@users.noreply.github.com>
2024-10-10 08:18:22 +0200

    Initial commit

---
+++ src/test/java/com/example/starshipsapp/StarshipsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starshipsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarshipsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
