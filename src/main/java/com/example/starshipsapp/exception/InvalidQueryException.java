50bd9626a41e7ba3882f2e8f619de4d34cb62b0a
do ar <118741935+dom-arm@users.noreply.github.com>
2024-10-11 15:07:03 +0200

    Warning fix

--- src/main/java/com/example/starshipsapp/exception/InvalidQueryException.java
+++ src/main/java/com/example/starshipsapp/exception/InvalidQueryException.java
@@ -2,6 +2,8 @@ package com.example.starshipsapp.exception;
 
 public class InvalidQueryException extends RuntimeException {
 
+	private static final long serialVersionUID = 8636315545642109845L; // Bc it's a Serializable
+
 	public InvalidQueryException(String sort, String order) {
 		super("An invalid query value: \"" + sort + "\" or \"" + order + "\"");
 	}
