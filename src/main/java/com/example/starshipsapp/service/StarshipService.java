fbd0237af9c8b77e8f22ebcc2f015fc491d1e9ab
do ar <118741935+dom-arm@users.noreply.github.com>
2024-10-11 13:46:15 +0200

    Sorting mechanism

--- src/main/java/com/example/starshipsapp/service/StarshipService.java
+++ src/main/java/com/example/starshipsapp/service/StarshipService.java
@@ -1,12 +1,67 @@
 package com.example.starshipsapp.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.starshipsapp.Starship;
 
 @Service
 public class StarshipService {
 
-	// Gets external data service injected
+	private final List<Starship> data;
 
-	// sort
+	public StarshipService(ExternalDataService externalDataService) {
+		this.data = externalDataService.fetchData();
+	}
 
+	public List<Starship> getData() {
+		return this.data;
+	}
+
+	public List<Starship> sortBy(String sort, String order) {
+
+		switch (sort) {
+		case "price":
+			List<Starship> sorted = sortByPrice(order);
+			return sorted.size() > 10 ? sorted.subList(0, 10) : sorted;
+		default:
+			// Throw not sorting on this yet
+			return null;
+		}
+
+	}
+
+	private List<Starship> sortByPrice(String order) {
+
+		switch (order) {
+		case "desc":
+			return sortInDescendingOrder(this.getData());
+		case "asc":
+			// Nothing implemented yet
+			return null;
+		default:
+			// Not ordering on this
+			return null;
+		}
+
+	}
+
+	private List<Starship> sortInDescendingOrder(List<Starship> data) {
+
+		data.sort((first, second) -> {
+			String price1 = first.cost_in_credits();
+			String price2 = second.cost_in_credits();
+
+			if (price1.equals("unknown"))
+				return 1;
+			if (price2.equals("unknown"))
+				return -1;
+
+			// desc
+			return Long.compare(Long.parseLong(price2), Long.parseLong(price1));
+		});
+
+		return data;
+	}
 }
